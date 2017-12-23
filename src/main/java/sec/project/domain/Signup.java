fde84917dad2f26519f6831e26b422e565eb3912
Laura Ojala <laojala@gmail.com>
2017-12-23 17:45:30 +0200

    Add database id. Remove test.

--- src/main/java/sec/project/domain/Signup.java
+++ src/main/java/sec/project/domain/Signup.java
@@ -1,12 +1,24 @@
 package sec.project.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
+@Table(name = "Signup")
 public class Signup extends AbstractPersistable<Long> {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "message")
     private String message;
 
     public Signup() {
@@ -19,6 +31,10 @@ public class Signup extends AbstractPersistable<Long> {
         this.message = message;
     }
 
+    public Long getId() {
+        return id;
+    }
+    
     public String getName() {
         return name;
     }
